3351d23f704fb0f645d3705d8e386d29a4ec27e8
touatidhafer <dhafer127@gmail.com>
2019-08-19 02:48:51 +0100

    Updates

---
+++ precache-manifest.140c9a69eb5ff8b461c997c1e04b86f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7665b1a80b7bc0063b5d472f3edbee9e",
+    "url": "/movieapp-react/index.html"
+  },
+  {
+    "revision": "1038f7133dad8624a734",
+    "url": "/movieapp-react/static/css/main.c53e4e0d.chunk.css"
+  },
+  {
+    "revision": "e49c5017ff5778d1b97b",
+    "url": "/movieapp-react/static/js/2.3de6b9f2.chunk.js"
+  },
+  {
+    "revision": "1038f7133dad8624a734",
+    "url": "/movieapp-react/static/js/main.5bc6073c.chunk.js"
+  },
+  {
+    "revision": "4fc06a36d104b403ab43",
+    "url": "/movieapp-react/static/js/runtime~main.a0fd5af1.js"
+  }
+]);
